8e3810f27164926475cde5d758acba68580864b9
Nejat ARINIK <arinikn@hotmail.com>
2017-10-01 19:53:28 +0200

    minor change - helloworld2

--- src/helloworld/Helloworld2.java
+++ src/helloworld/Helloworld2.java
@@ -5,6 +5,7 @@ public class Helloworld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World22!!");
+		System.out.println("Hello World22!!");
 	}
 
 }
